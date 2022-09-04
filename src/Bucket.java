48e19cd2cac91f23f7e48858afef9c1b663c01fb
OlyaTelesh <telesh.olya@mail.ru>
2022-09-04 21:14:36 +0300

    создали три метода для добавления фруктов, для удаления с начала списка и для показа всей колекции.

--- src/Bucket.java
+++ src/Bucket.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Bucket {
+
+    ArrayList<String> fruits = new ArrayList<>();
+    String fruit;
+
+    public String putFruit(String fruit) {
+        fruits.add(fruit);
+        this.fruit=fruit;
+        return fruit;
+    }
+
+    public String takeFruit (String fruit) {
+        fruits.remove(0);
+        return fruit;
+    }
+
+    public void showFruits () {
+        System.out.println(fruits);
+    }
 }
+
